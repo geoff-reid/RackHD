961d1a2c5817dab970fe1344fd09e688d80f8ef7
Geoff Reid <geoffrey.reid@dell.com>
2017-09-26 15:22:58 -0400

    WIP

--- example/docker/infrasim/start_node.sh
+++ example/docker/infrasim/start_node.sh
@@ -1,26 +1,29 @@
-#!/bin/bash
+#!/bin/bash -x
 
 CONTAINER=infrasim_1
-INTERFACE=eth1
+INTERFACE=eth0
 BRIDGE=br0
 
 # create the infrasim container
-docker create -it --net=docker_southbound --privileged --name $CONTAINER infrasim/infrasim-compute:3.5.1
+#docker create -it --net=docker_southbound --privileged --name $CONTAINER infrasim/infrasim-compute:3.5.1
 
 # add the admin network to it
-docker network connect docker_admin infrasim_1
+#docker network connect docker_admin infrasim_1
 
 # start the container
-docker start $CONTAINER
+#docker start $CONTAINER
 
 # copy the vnode config to the container
-docker cp default.yml $CONTAINER:/root/.infrasim/.node_map
+#docker cp default.yml $CONTAINER:/root/.infrasim/.node_map
 
 # get the IP for the bridge 
-IP=$(docker exec $CONTAINER ifconfig $INTERFACE | awk '/inet addr/{print substr($2,6)}')
+#IP=$(docker exec $CONTAINER ifconfig $INTERFACE | awk '/inet addr/{print substr($2,6)}')
+
+IP=$(ifconfig $INTERFACE | awk '/inet addr/{print substr($2,6)}')
+echo $IP
 
 # config the bridge interface and start the virtual node
-docker exec -i $CONTAINER bash <<EOF
+#docker exec -i $CONTAINER bash <<EOF
 ifconfig $INTERFACE 0.0.0.0
 ifconfig $INTERFACE promisc
 brctl addbr br0
@@ -32,6 +35,6 @@ ifconfig br0 $IP
 ifconfig br0 up
 ifconfig
 infrasim node start
-EOF
+#EOF
 
 
